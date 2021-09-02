89621de162d35048f813c1c6f18218dd186e4955
Dmitrii-Tr <84545898+Dmitrii-Tr@users.noreply.github.com>
2021-09-02 16:58:27 +0300

    Починил кнопку закрыть в Jquery

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
+
 $(document).ready(function () {
-	$('.menu__btn').on('click', function () {
+	$('.menu__btn, .btn__hide').on('click', function () {
 		$('.menu').toggleClass("menu--active")
 		$('.menu__list-logo').toggleClass("menu--logo")
-		$('.menu__btn').toggleClass("btn--hiden")
-		$('.menu__btn--close').toggleClass("menu__btn--active")	
+		$('.menu__btn').toggleClass("btn__hiden")
+		$('.btn__hide').toggleClass("btn__visible")
 	});
-	 $('.menu__btn--close').on('click', function () {
-		 $('.menu,.menu__btn-close').hide()
-	 });
-
-
 });
 
+
+
+
+
+
+// $('.menu__btn').removeClass("menu__btn").addClass("btn--hide")
+
+
+
+
+// $(.)
+// let isOpen = false; // Задаем переменную в которой будем хранить состояние
+// $('.item-contacts__title').on('click',function(){
+//   $(this).next().toggle('slow');
+//   $(this).parent().parent().css('top', (isOpen ? '70%' : 0)); // В зависимости от состояния задаем позицию
+//   isOpen = !isOpen; // Меняем состояние на противоположное
+// })
